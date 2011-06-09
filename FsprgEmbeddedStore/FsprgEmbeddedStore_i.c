846a2ffeba2c47140ad62c82fc695999cfad2e68
brumple <brumple@comcast.net>
2011-06-09 13:13:06 -0400

    Update project files

--- FsprgEmbeddedStore/FsprgEmbeddedStore_i.c
+++ FsprgEmbeddedStore/FsprgEmbeddedStore_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue May 31 17:22:56 2011
+/* at Thu Jun 09 10:23:56 2011
  */
 /* Compiler settings for FsprgEmbeddedStore.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
